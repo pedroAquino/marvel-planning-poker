e133e0982d3c416b03d5bac13444b988ecd688f0
Pedro Aquino <pedro@morethanmetrics.com>
2021-03-06 16:15:17 +0100

    configuring chakra theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '@fontsource/roboto-condensed';
+
+const theme = extendTheme({
+  fonts: {
+    body: 'Roboto Condensed',
+    heading: 'Roboto Condensed',
+    mono: 'Roboto Condensed'
+  }
+});
 
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/components.tsx
+++ pages/components.tsx
@@ -1,11 +1,17 @@
 import Button from '../components/Button';
+import { Box, Heading } from '@chakra-ui/react';
 
 function Components() {
   return (
-    <>
-      <h1>Components</h1>
-      <Button>Start a new session</Button>
-    </>
+    <Box p="5">
+      <Heading mb="5" >COMPONENTS PAGE</Heading>
+      <Box>
+        <Heading size="sm">Logo Component</Heading>
+      </Box>
+      <Box>
+        <Heading size="sm">Button Component</Heading>
+      </Box>
+    </Box>
   );
 }
 
