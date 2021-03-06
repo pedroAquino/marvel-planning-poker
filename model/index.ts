1e672a53d3eedfc9f4afcac8fb9670d9d2c91224
Pedro Aquino <pedro@morethanmetrics.com>
2021-03-06 17:07:08 +0100

    adding models for poker card

--- model/index.ts
+++ model/index.ts
@@ -10,19 +10,39 @@ export interface IUser {
   role: UserRole;
 }
 
+export const User = ({ name = '', role = UserRole['player'] } = {}): IUser => ({
+  name,
+  role
+});
+
 export interface ISession  {
   id: string;
   creator: IUser;
   users: IUser[];
 }
 
-export const User = ({ name = '', role = UserRole['player'] } = {}): IUser => ({
-  name,
-  role
-});
-
 export const Session = ({id = uuid(), creator = User(), users = []} = {}): ISession => ({
   id,
   creator,
   users
+});
+
+export interface IMarvelCharacter {
+  name: string;
+  image: string;
+}
+
+export interface IPokerCard {
+  marvelCharacter: IMarvelCharacter;
+  size: '1' | '2' | '3' | '5' | '8' | '13';
+}
+
+export const MarvelCharacter = ({ name = '', image = '' } = {}): IMarvelCharacter => ({
+  name,
+  image
+});
+
+export const PokerCard = ({ marvelCharacter = MarvelCharacter(), size }): IPokerCard => ({
+  marvelCharacter,
+  size
 });
