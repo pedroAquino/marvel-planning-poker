7069c287adca09d5fb4daecc921907648b71b863
Pedro Aquino <pedro@morethanmetrics.com>
2021-03-07 19:51:19 +0100

    extracting dimensions logic from PokerCard

--- components/PokerCard.tsx
+++ components/PokerCard.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Heading } from '@chakra-ui/react';
 import Image from 'next/image';
+import getDimensions from './helpers/getDimensions';
 
 interface PokerCardProps {
   imgSrc: string;
@@ -13,20 +14,6 @@ const largeDimension = {
   height: 450
 };
 
-const decreaseBy = (factor: number, value: number) => value / factor;
-
-const getCardDimensions = (size) => ({
-  sm: { 
-    width: decreaseBy(2, largeDimension.width),
-    height: decreaseBy(2, largeDimension.height)
-  },
-  md: { 
-    width: decreaseBy(1.5, largeDimension.width),
-    height: decreaseBy(1.5, largeDimension.height)
-  },
-  lg: largeDimension
-}[size])
-
 const CartTitle = ({ children }) => {
   return (
   <Heading size="md" p="1" textTransform="uppercase">
@@ -36,7 +23,7 @@ const CartTitle = ({ children }) => {
 };
 
 export default function PokerCard({ imgSrc, bottomTitle, title, size = 'md' }: PokerCardProps) {
-  const dimensions = getCardDimensions(size);
+  const dimensions = getDimensions(largeDimension, size);
   return (
     <Box p="5" color="white" bg="black" borderRadius="lg">
       <CartTitle>{title}</CartTitle>
---
+++ components/helpers/getDimensions.ts
@@ -0,0 +1,20 @@
+interface Dimension {
+  width: number;
+  height: number;
+}
+
+const decreaseBy = (factor: number, value: number) => value / factor;
+
+export const getDimensions = (largestDimension: Dimension, size: 'sm' | 'md' | 'lg'): Dimension => ({
+  sm: { 
+    width: decreaseBy(2, largestDimension.width),
+    height: decreaseBy(2, largestDimension.height)
+  },
+  md: { 
+    width: decreaseBy(1.5, largestDimension.width),
+    height: decreaseBy(1.5, largestDimension.height)
+  },
+  lg: largestDimension
+}[size]);
+
+export default getDimensions;
