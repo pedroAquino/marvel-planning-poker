5a92f95caa38d89297cecd806703660cec96ea05
Pedro Aquino <pedro@morethanmetrics.com>
2021-04-03 17:49:15 +0200

    #5 basic sequelize configuration

---
+++ db/config.js
@@ -0,0 +1,16 @@
+
+require('dotenv').config();
+
+const config = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: "mysql"  
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config
+};
