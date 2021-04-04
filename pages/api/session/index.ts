3c9d9b443195458d12db7e669b876bc6093a1fa2
Pedro Aquino <pedro@morethanmetrics.com>
2021-04-04 16:12:04 +0200

    #5 adding more accurate logs

--- pages/api/session/index.ts
+++ pages/api/session/index.ts
@@ -29,16 +29,16 @@ export default async function handler(req, res) {
 
   console.log('CONFIG >>>>>>>>>> ', sequelize.config);
 
-  try {
-    await sequelize.authenticate();
-  } catch (error) {
-    console.error('Not Connected', error);
-    const errResponse = {
-      stack: error
-    };
-    res.status(500).json(errResponse);
-  }
-  await sequelize.close();
+  // try {
+  //   await sequelize.authenticate();
+  // } catch (error) {
+  //   console.error('Not Connected', error);
+  //   const errResponse = {
+  //     stack: error
+  //   };
+  //   res.status(500).json(errResponse);
+  // }
+  // await sequelize.close();
 
-  res.status(200).json(response);
+  res.status(200).json(sequelize.config);
 };
